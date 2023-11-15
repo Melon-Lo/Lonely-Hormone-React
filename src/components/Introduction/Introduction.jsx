b1b6db0a1638d8dbde6cf5c7a09a250ca220f220
Melon-Lo <dp98062@gmail.com>
2023-11-15 23:04:56 +0800

    feat/ update Introduction

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -13,12 +13,12 @@ function Introduction() {
   const characters = charactersData.map(character => (
     <div key={character.id} className='characterBox'>
       <img src={character.line} alt={character.name + 'LineBg'} className='lineBg' />
-      <div className={'headBox ' + clsx({left: character.id % 2 !== 0, right: character.id % 2 === 0})}>
+      <div className={'headBox ' + clsx({'left fadeInLeft': character.id % 2 !== 0, 'right fadeInRight': character.id % 2 === 0})}>
         <img src={character.avatar} alt={character.name + 'Avatar'} className='head'/>
       </div>
       <div className={'introductionBox ' + clsx({right: character.id % 2 !== 0, left: character.id % 2 === 0})}>
         <div className="name">{character.name}</div>
-        <div className="slogan">{character.slogan}</div>
+        <div className="slogan typing">{character.slogan}</div>
         <div className="contentBox">
           <img src={background} alt="background" className='bg' />
           <div className="content">{character.introduction}</div>
