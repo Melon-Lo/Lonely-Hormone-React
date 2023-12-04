fb14fb25ddbbadafd5244f39164b7c01100fd771
Melon-Lo <dp98062@gmail.com>
2023-12-04 19:12:27 +0800

    feat/ rwd InfoBox

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,8 +1,14 @@
 import './InfoBox.scss'
 
+// import hook
+import { useContext } from 'react'
+import { WindowWidthContext } from 'context/WindowWidthContext'
+
 export default function InfoBox({content, img}) {
+  const { windowWidth } = useContext(WindowWidthContext)
+
   return (
-    <div className="infoBoxContainer">
+    <div className={windowWidth > 768 ? "infoBoxContainer" : "smallInfoBoxContainer"}>
       <img 
         className="img" 
         src={img} 
